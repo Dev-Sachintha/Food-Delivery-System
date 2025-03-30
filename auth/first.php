36c4c29c5954b44b1df4ec51ce6fb73f7da11ca1
Dev-Sachintha <chamikara24sachintha@gmail.com>
2025-03-30 12:04:35 +0530

    initial commit

---
+++ auth/first.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-compatible" content="IE-edge">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>single product page Design</title>
+    <link rel="stylesheet" herf="./css/firstpage.css">
+</head>
+<body>
+    <section class="main-wrap">
+        <div class="product">
+            <div class="image-gallery">
+             <img src="pixsels.jpg" id="producting" alt="">
+             <div class="controls">
+                <span class="btn"></span>
+                <span class="btn"></span>
+                <span class="btn"></span>
+             </div>
+            </div>
+            <div class="product-details">
+                <div class="details"></div>
+            </div>
+        </div>
+    </section>
+    <script src="./js/firstpage.js"></script>
+</body>
+
+
+
+
+
+</html>
